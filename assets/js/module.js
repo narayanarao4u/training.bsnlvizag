dcd0a4209938aba2b0b5af870efe7ca76ac3ff1a
Narayana Rao <narayanarao4u@yahoo.com>
2022-09-08 21:51:24 +0530

    server change to http://117.239.146.106:3000/

--- assets/js/module.js
+++ assets/js/module.js
@@ -1,6 +1,7 @@
 const app = angular.module('myApp',['ngRoute','toaster', 'ngAnimate', 'ngMaterial', 'ui.bootstrap']);
 //const baseURL = "http://localhost:3000/api";
-const baseURL = "https://bsnl-training.herokuapp.com/api";
+// const baseURL = "https://bsnl-training.herokuapp.com/api";
+const baseURL = "http://117.239.146.106:3000/api";
 
 app.run(function($rootScope){
     $rootScope.login = {};
